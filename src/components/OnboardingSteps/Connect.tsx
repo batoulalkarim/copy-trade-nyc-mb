0cb89e94c6ca283959cac3cbe93801d3bc7cc057
Batoul Alkarim <batoulalkarim1@gmail.com>
2023-09-23 14:00:26 -0400

    privy twitter integration

--- src/components/OnboardingSteps/Connect.tsx
+++ src/components/OnboardingSteps/Connect.tsx
@@ -1,31 +1,37 @@
-import { styled } from 'styled-components'
-import { Button } from '../Button'
+import { styled } from "styled-components";
+import { Button } from "../Button";
+import { useRouter } from "next/router";
+import { usePrivy } from "@privy-io/react-auth";
 
 export function Connect({ handleNext }: { handleNext: () => void }) {
-	return (
-		<Container>
-			<Title>COPYTRADE</Title>
-			<Subtitle>{`Use X or your MetaMask to connect.`}</Subtitle>
-			<Button action={handleNext} text='Login with X' />
-		</Container>
-	)
+  //   function login() {
+  //     window.location.href = "/api/twitter-auth";
+  //   }
+  const { linkTwitter } = usePrivy();
+  return (
+    <Container>
+      <Title>COPYTRADE</Title>
+      <Subtitle>{`Use X or your MetaMask to connect.`}</Subtitle>
+      <Button action={linkTwitter} text="Login with X" />
+    </Container>
+  );
 }
 
 const Container = styled.div`
-	align-items: center;
-	display: flex;
-	flex-direction: column;
-	margin: 24px auto;
-	gap: 8px;
-`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  margin: 24px auto;
+  gap: 8px;
+`;
 
 const Title = styled.div`
-	font-size: 24px;
-	line-height: 1.5;
-`
+  font-size: 24px;
+  line-height: 1.5;
+`;
 
 const Subtitle = styled.div`
-	color: #6a6a6a;
-	font-size: 16px;
-	line-height: 1.5;
-`
+  color: #6a6a6a;
+  font-size: 16px;
+  line-height: 1.5;
+`;
