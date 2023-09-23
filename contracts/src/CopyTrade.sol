b675eb765b60c4ee93e1411e2ab1b1eee39f867f
mehranhydary <mehranhydary@users.noreply.github.com>
2023-09-23 09:15:38 -0400

    Added base contract

--- contracts/src/CopyTrade.sol
+++ contracts/src/CopyTrade.sol
@@ -1,7 +1,39 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.19;
 
-import {BaseHook} from "lib/periphery-next/contracts/BaseHook.sol";
-import {IPoolManager} from "lib/v4-core/interfaces/IPoolManager.sol";
+import {BaseHook} from "periphery-next/BaseHook.sol";
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+import {PoolId, PoolIdLibrary} from "v4-core/types/PoolId.sol";
 
-contract CopyTrade is BaseHook {}
+contract CopyTrade is BaseHook {
+    using PoolIdLibrary for PoolId;
+
+    uint256 public beforeSwapCount;
+
+    constructor(IPoolManager _poolManager) BaseHook(_poolManager) {}
+
+    function getHooksCalls() public pure override returns (Hooks.Calls memory) {
+        return
+            Hooks.Calls({
+                beforeInitialize: false,
+                afterInitialize: false,
+                beforeModifyPosition: false,
+                afterModifyPosition: false,
+                beforeSwap: false,
+                afterSwap: true, // Start with just this
+                beforeDonate: false,
+                afterDonate: false
+            });
+    }
+
+    function afterSwap(
+        address,
+        PoolKey calldata,
+        IPoolManager.SwapParams calldata,
+        BalanceDelta,
+        bytes calldata
+    ) external override returns (bytes4) {
+        beforeSwapCount++;
+        return BaseHook.beforeSwap.selector;
+    }
+}
--- contracts/test/CopyTrade.t.sol
+++ contracts/test/CopyTrade.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.19;
 
-import {Test, console2} from "lib/forge-std/src/Test.sol";
+import {Test, console2} from "forge-std/Test.sol";
 import {CopyTrade} from "../src/CopyTrade.sol";
 
 contract CopyTradeTest is Test {
