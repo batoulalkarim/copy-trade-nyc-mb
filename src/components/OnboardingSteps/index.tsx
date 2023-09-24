34dbb3aa8e399781edd220692174f83b436e5913
Batoul Alkarim <batoulalkarim1@gmail.com>
2023-09-23 21:59:00 -0400

    fixed privy handleback error

--- src/components/OnboardingSteps/index.tsx
+++ src/components/OnboardingSteps/index.tsx
@@ -1,44 +1,38 @@
-import { useState } from 'react'
-import { useRouter } from 'next/router'
-import { Connect } from './Connect'
-import { PrivyNotice } from './PrivyNotice'
-import { Deposit } from './Deposit'
-import { AddToWatchList } from './AddToWatchList'
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { Connect } from "./Connect";
+import { PrivyNotice } from "./PrivyNotice";
+import { Deposit } from "./Deposit";
+import { AddToWatchList } from "./AddToWatchList";
 
 type OnboardingSteps =
-	| 'CONNECT'
-	| 'PRIVY_NOTICE'
-	| 'DEPOSIT'
-	| 'ADD_TO_WATCH_LIST'
+  | "CONNECT"
+  | "PRIVY_NOTICE"
+  | "DEPOSIT"
+  | "ADD_TO_WATCH_LIST";
 
 export function OnboardingSteps() {
-	const router = useRouter()
-	const [step, setStep] = useState<OnboardingSteps>('CONNECT')
+  const router = useRouter();
+  const [step, setStep] = useState<OnboardingSteps>("CONNECT");
 
-	const handleStep = (step: OnboardingSteps) => {
-		setStep(step)
-	}
+  const handleStep = (step: OnboardingSteps) => {
+    setStep(step);
+  };
 
-	return (
-		<div>
-			{step === 'CONNECT' && (
-				<Connect handleNext={() => handleStep('PRIVY_NOTICE')} />
-			)}
-			{step === 'PRIVY_NOTICE' && (
-				<PrivyNotice
-					handleNext={() => handleStep('DEPOSIT')}
-					handleBack={() => handleStep('CONNECT')}
-				/>
-			)}
-			{step === 'DEPOSIT' && (
-				<Deposit
-					handleNext={() => handleStep('ADD_TO_WATCH_LIST')}
-					handleBack={() => handleStep('PRIVY_NOTICE')}
-				/>
-			)}
-			{step === 'ADD_TO_WATCH_LIST' && (
-				<AddToWatchList handleSkip={() => router.push('/dashboard')} />
-			)}
-		</div>
-	)
+  return (
+    <div>
+      {step === "CONNECT" && (
+        <Connect handleNext={() => handleStep("PRIVY_NOTICE")} />
+      )}
+      {step === "PRIVY_NOTICE" && (
+        <PrivyNotice handleNext={() => handleStep("DEPOSIT")} />
+      )}
+      {step === "DEPOSIT" && (
+        <Deposit handleNext={() => handleStep("ADD_TO_WATCH_LIST")} />
+      )}
+      {step === "ADD_TO_WATCH_LIST" && (
+        <AddToWatchList handleSkip={() => router.push("/dashboard")} />
+      )}
+    </div>
+  );
 }
